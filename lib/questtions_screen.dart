57f2b4a51386d12cb70275479fe584ae41aa0b32
Felex Kuria <engineerfelex@gmail.com>
2023-08-22 12:17:41 +0300

    added c++ engine

---
+++ lib/questtions_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'package:quiz_app/question_button.dart';
+import 'package:quiz_app/questions.dart';
+
+class QuestionScreen extends StatefulWidget {
+  const QuestionScreen({super.key});
+
+  @override
+  State<QuestionScreen> createState() => _QuestionScreenState();
+}
+
+class _QuestionScreenState extends State<QuestionScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    if (currentQuestionIndex <= questions.length) {
+      setState(() {
+        currentQuestionIndex++;
+      });
+    } else {
+      setState(() {
+        currentQuestionIndex == 0;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final currentQuestion = questions[currentQuestionIndex];
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.symmetric(
+            vertical: 10, horizontal: kIsWeb ? 1080 / 1.8 : 100),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.questionText,
+              textAlign: TextAlign.center,
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffled().map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onPressed: answerQuestion,
+              );
+            }),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/quiz.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questtions_screen.dart';
+
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+  @override
+  void initState() {
+    activeScreen = StartScreen(startQuiz: changeScreen);
+    super.initState();
+  }
+
+  void changeScreen() {
+    setState(() {
+      activeScreen = const QuestionScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        //  colorScheme: lightColorScheme,
+      ),
+      home: Scaffold(
+        body: Container(
+          width: double.infinity,
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                Color.fromARGB(252, 81, 0, 155),
+                Color.fromARGB(252, 107, 15, 168),
+              ])),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
